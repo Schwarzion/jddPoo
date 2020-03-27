ee2e92e34c010db6b61855e29f886fb2638c2306
Schwarzion <victor.lehouck@gmail.com>
2020-03-27 12:15:45 +0100

    Init

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+//TODO
+
+// import { Partie } from "./classes/partie";
+
+// const partie : Partie = new Partie(5,4);
+
+// partie.initialiser();
+// partie.lancer();
+// partie.afficher_gagnant();
